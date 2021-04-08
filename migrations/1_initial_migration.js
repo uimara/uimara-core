24c5746ef1050fbb749564b97481bc70a0e85aa8
Chukwuemeka Nwagu <developer.nwagu@gmail.com>
2021-04-08 11:11:55 +0100

    add libraries and initial code from ampleforth

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Uimara = artifacts.require("Uimara");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Uimara);
 };
